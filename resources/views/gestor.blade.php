bd859c9e08e6cab65d40d6dbc0e9632f7eb58eef
Bryan <bryansilva021@hotmail.com>
2019-09-18 19:22:26 -0500

    Creación de sección para la creación de preguntas

--- resources/views/gestor.blade.php
+++ resources/views/gestor.blade.php
@@ -1,31 +1,80 @@
 @extends('layouts.app')
+@section('css')
+  <style>
+      .contenido {
+          background-color: #e6e8e8;
+          padding: 45px;
+          border: 25px;
+          margin-top: 15px;
+          box-shadow: 9px 9px 13px #0000006b;
+      }
+  </style>
+@endsection
 @section('content')
-    <div style="background-color: #f9f9f9;padding: 45px; border: 25px; margin-top: 15px" class="container">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <select class="form-control">
-                    <option>Test de aptitud</option>
-                    <option>Test de logica</option>
-                </select>
-                <br>
-                <select class="form-control">
-                    <option>nivel basico</option>
-                    <option>nivel intermedio</option>
-                    <option>nivel final</option>
-                </select>
-                <br>
-                <label class="form-control-label">Ingresar pregunta</label>
-                <input class="form-control" placeholder="ingrese su pregunta">
-                <hr>
-                <label class="form-control-label">Ingrese sus opciones</label>
-                <input class="form-control" placeholder="pregunta">
+    <div class="container contenido">
+        <div class="row justify-content-md-center">
+            <div class="col-md-8">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link " id="tap-gestionar" data-toggle="tab" href="#gestionar" role="tab" aria-controls="gestionar" aria-selected="true">Gestionar preguntas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="tap-agregar" data-toggle="tab" href="#agregar" role="tab" aria-controls="agregar" aria-selected="false">Agregar preguntas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="tap-consultar" data-toggle="tab" href="#consultar" role="tab" aria-controls="consultar" aria-selected="false">Intentos por test</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    {{-- sección para gestionar preguntas --}}
+                    <div class="tab-pane fade" id="gestionar" role="tabpanel" aria-labelledby="gestionar-tab">...</div>
+                    {{-- Sección para agregar preguntas --}}
+                    <div class="tab-pane fade show active" id="agregar" role="tabpanel" aria-labelledby="agregar-tab">
+                        <div class="row justify-content-md-center">
+                            <div class="col-md-10 mt-5">
+                                <select class="form-control" id="tipo_test">
+                                    <option disabled selected> -- Tipo del test --</option>
+                                    <option>Test de aptitud</option>
+                                    <option>Test de logica</option>
+                                </select>
+                                <br>
+                                <select class="form-control" id="nivel_test">
+                                    <option disabled selected> -- Nivel del test --</option>
+                                    <option>Basico</option>
+                                    <option>Intermedio</option>
+                                    <option>Avanzado</option>
+                                </select>
+                                <br>
+                                <label class="form-control-label"  for="pregunta">Ingresar pregunta  </label>
+                                <input class="form-control" id="pregunta" placeholder="ingrese su pregunta">
+                                <hr>
+                                <label class="form-control-label" for="opcion_test">
+                                    Ingrese sus opciones <span id="total_preguntas" class="badge badge-pill badge-info">5</span>
+                                    <span class="text-warning"> <small>!maximo 5 opciones</small> </span>
+                                </label>
+                                <input class="form-control" id="opcion_test" placeholder="Ingrese su opción">
 
-                <button style="margin-top: 20px;" class="btn btn-primary">Agregar opción</button>
-                <hr>
-                <label class="form-control-label">Seleccione la pregunta correcta</label>
+                                <button style="margin-top: 20px;" class="btn btn-primary">Agregar opción</button>
+                                <hr>
+                                <label class="form-control-label">Seleccione la pregunta correcta</label>
+                                <fieldset>
+                                    <legend>Seleccione la opción correcta</legend>
+                                    <input type="radio">
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- Sección para consultar intentos por test --}}
+                    <div class="tab-pane fade" id="consultar" role="tabpanel" aria-labelledby="consultar-tab">...</div>
+                </div>
             </div>
         </div>
     </div>
+@endsection
 
+@section('js')
+    <script>
+
+    </script>
 @endsection
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    @yield('css')
 </head>
 <body>
 @auth
@@ -106,6 +108,6 @@
                 @yield('content')
         </main>
     </div>
-
+    @yield('script')
 </body>
 </html>
